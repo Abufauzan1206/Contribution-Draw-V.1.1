1dbb3e339930b29f72dafe75a47c88ae494a8231
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-04 05:30:19 +0100

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Authentication Module
+// =====================================================
+
+console.log("Authentication Module Loaded");
