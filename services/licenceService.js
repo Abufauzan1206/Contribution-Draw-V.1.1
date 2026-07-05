057ee204e193c530f58e86e4cf86684f014680e4
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 17:49:19 +0100

    Update licenceService.js

--- services/licenceService.js
+++ services/licenceService.js
@@ -1,1 +1,25 @@
+import {
+    createDocument,
+    queryDocuments
+} from "../firebase/firestore.js";
 
+/**
+ * Create a license
+ */
+export async function createLicense(data) {
+    return await createDocument("licenses", data);
+}
+
+/**
+ * Validate license
+ */
+export async function validateLicense(organizationId) {
+    const result = await queryDocuments(
+        "licenses",
+        "organizationId",
+        "==",
+        organizationId
+    );
+
+    return result;
+}
