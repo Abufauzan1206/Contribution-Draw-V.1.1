2c019091ad093f5cd708f706a51a545a5e99dfd3
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 10:20:15 +0100

    CMP-015: refactor create member management module

---
+++ members.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Member Management Module
+// =====================================================
+
+console.log("Member Management Module Loaded");
