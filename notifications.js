bd44bcde6195427bf5b03d61038849679115beda
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 10:40:50 +0100

    CMP-021: refactor create notification module

---
+++ notifications.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Notification Module
+// =====================================================
+
+console.log("Notification Module Loaded");
