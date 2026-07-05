3f4a3e25331625d9972ea58e7d78eac0c026a7e6
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 10:08:45 +0100

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Administration Module
+// =====================================================
+
+console.log("Administration Module Loaded");
