25a71f7b903967784cb69f271114bb31f3d7b947
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 06:10:54 +0100

    CMP-011: refactor register splash module with main controller

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 import { auth } from "./auth.js";
 import "./drawEngine.js";
 import "./navigation.js";
+import "./splash.js";
 
 console.log("CMP Main Application Loaded");
 console.log("Authentication Ready:", auth);
