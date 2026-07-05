9bc8c5eafce7f2c03232b9298f6e4ba4edd443f2
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 06:00:20 +0100

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Navigation Module
+// =====================================================
+
+console.log("Navigation Module Loaded");
