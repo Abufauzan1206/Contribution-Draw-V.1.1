54e90047e0da33a4dcb7310103464b545b63dc5d
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 17:48:14 +0100

    Update organizationService.js

--- services/organizationService.js
+++ services/organizationService.js
@@ -1,1 +1,29 @@
+import {
+    createDocument,
+    getDocument,
+    updateDocument
+} from "../firebase/firestore.js";
 
+/**
+ * Create a new organization
+ */
+export async function createOrganization(data) {
+    return await createDocument("organizations", {
+        status: "active",
+        ...data
+    });
+}
+
+/**
+ * Get an organization
+ */
+export async function getOrganization(id) {
+    return await getDocument("organizations", id);
+}
+
+/**
+ * Update organization
+ */
+export async function updateOrganization(id, data) {
+    return await updateDocument("organizations", id, data);
+  }
