4cafc2cd12f2d22801ac86692720a238459bafb6
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-05 10:57:32 +0100

    CMP-027: refactor create settings module

---
+++ settings.js
@@ -0,0 +1,6 @@
+// =====================================================
+// ABUFAUZAN TECH Cooperative Management Platform (CMP)
+// Settings Module
+// =====================================================
+
+console.log("Settings Module Loaded");
