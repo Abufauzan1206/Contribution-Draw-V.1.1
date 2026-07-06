c4322bb1772f5d053cd67f4c02f23cf76700ad81
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-07-06 03:14:39 +0100

    Update organizations.js

--- platform/J's/organizations.js
+++ platform/J's/organizations.js
@@ -1,45 +1,12 @@
-import {
-    createOrganization
-} from "../../services/organizationService.js";
+alert("organizations.js loaded");
 
 const form = document.getElementById("organizationForm");
 
-form.addEventListener("submit", async (event) => {
-
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const organization = {
-
-        name: document.getElementById("organizationName").value,
-
-        email: document.getElementById("organizationEmail").value,
-
-        phone: document.getElementById("organizationPhone").value
-
-    };
-
-    try {
-
-        await createOrganization(organization);
-
-        document.getElementById("message").textContent =
-            "Organization registered successfully.";
-
-        form.reset();
-
-    } catch (error) {
-
-    console.error(error);
-
-    alert(
-        "Error: " +
-        error.code +
-        "\n\n" +
-        error.message
-    );
+    alert("Submit button clicked");
 
     document.getElementById("message").textContent =
-        error.message;
-
-}
+        "JavaScript is working.";
 });
